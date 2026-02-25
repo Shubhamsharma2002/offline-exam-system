abe668bbe1ed3f5b681930c7c5eda9d61ee60f4a
Shubham Sharma <shubhamtech004@gmail.com>
2026-02-25 22:53:14 +0530

    change under client
    instal recat router dom , setup routes , responsive navbar and footer section done

--- client/src/components/layouts/Navbar.jsx
+++ client/src/components/layouts/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu, X } from "lucide-react";
 
+
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
